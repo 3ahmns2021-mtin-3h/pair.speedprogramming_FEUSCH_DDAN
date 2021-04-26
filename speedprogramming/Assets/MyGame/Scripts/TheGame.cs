c21ab5bdacf8ae6114a4e627c77f1466ed50a006
Don_Denki <dantlerdavid@gmail.com>
2021-04-26 12:31:53 +0200

    First Code

---
+++ speedprogramming/Assets/MyGame/Scripts/TheGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheGame : MonoBehaviour
+{
+    public GameObject rose;
+
+    private int numSpace;
+    private int numK;
+    private float time;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            numSpace++;
+            time = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            numK++;
+        }
+    }
+}
